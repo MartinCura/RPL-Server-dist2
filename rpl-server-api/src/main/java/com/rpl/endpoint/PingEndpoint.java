8797cfcc412ede6a56d5de26027d7262a195953c
AlJuan <juan6691@gmail.com>
2016-09-26 10:38:02 -0300

    Se agrega soporte para JSON

--- rpl-server-api/src/main/java/com/rpl/endpoint/PingEndpoint.java
+++ rpl-server-api/src/main/java/com/rpl/endpoint/PingEndpoint.java
@@ -1,9 +1,12 @@
 package com.rpl.endpoint;
 
+import java.util.HashMap;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class PingEndpoint {
@@ -11,9 +14,33 @@ public class PingEndpoint {
 	@GET
 	@Path("/ping")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String printMessage() {
+	public String ping() {
 
 		return "pong";
+
+	}
+	
+	@GET
+	@Path("/pingJson")
+	@Produces(MediaType.APPLICATION_JSON)
+	public HashMap<String, String> pingJson() {
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("Response", "pong");
+		
+		return map;
+
+	}
+	
+	@GET
+	@Path("/pingJsonStatus")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response pingJsonStatus() {
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("Response", "pong");
+		
+		return Response.status(200).entity(map).build();
 
 	}
 
