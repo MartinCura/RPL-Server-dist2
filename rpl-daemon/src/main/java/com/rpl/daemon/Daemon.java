9fb0cabab0dc3adf080981fcbed896fad594494c
kevinmlew <kevin.martin.lew@gmail.com>
2016-11-05 11:58:45 -0300

    Daemon ejecuta el runner y guarda salida en disco

--- rpl-daemon/src/main/java/com/rpl/daemon/Daemon.java
+++ rpl-daemon/src/main/java/com/rpl/daemon/Daemon.java
@@ -1,18 +1,31 @@
 package com.rpl.daemon;
 
+import com.rpl.model.ActivitySubmission;
+import com.rpl.model.QueueMessage;
 import com.rpl.service.QueueService;
 import com.rpl.serviceImpl.QueueServiceImpl;
 
 public class Daemon {
 	
-    public static void main( String[] args ){
+	public static void main( String[] args ){
         
-    	QueueService qs = new QueueServiceImpl();
+		QueueService qs = new QueueServiceImpl();
+		Tester tester = new Tester();
+		boolean running = true;
     	
-    	Producer producer = new Producer(qs);
-    	Consumer consumer = new Consumer(qs);
-    	
-    	producer.start();
-    	consumer.start();
-    }
+		while (running) {
+			try {
+				QueueMessage message = qs.receive();
+				String submissionId = message.getMsg();
+				//TODO buscar la submission de la db
+				ActivitySubmission submission = new ActivitySubmission();
+
+				String output = tester.runSubmission(submission);
+				String result = tester.analyzeOutput(submission, output);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
