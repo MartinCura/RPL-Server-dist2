02c2b96ce520dd3d2906245891af7192a2cf8447
AlJuan <juan6691@gmail.com>
2016-11-06 14:55:15 -0300

    Se agrega la funcionalidad de persistir en la base al enviar un codigo

--- rpl-persistence/src/main/java/com/rpl/persistence/ActivitySubmissionDAO.java
+++ rpl-persistence/src/main/java/com/rpl/persistence/ActivitySubmissionDAO.java
@@ -3,11 +3,19 @@ package com.rpl.persistence;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import com.rpl.model.DBTest;
+import com.rpl.model.ActivitySubmission;
 
 public class ActivitySubmissionDAO {
 	@PersistenceContext(unitName = "RplPU")
     private EntityManager entityManager;
 
+	public ActivitySubmission find(long id) {
+		return entityManager.find(ActivitySubmission.class, id);
+	}
+	
+	public ActivitySubmission save(ActivitySubmission act){
+		return entityManager.merge(act);
+	}
+	
 	
 }
--- rpl-service/src/main/java/com/rpl/serviceImpl/SolutionServiceImpl.java
+++ rpl-service/src/main/java/com/rpl/serviceImpl/SolutionServiceImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.concurrent.TimeoutException;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,10 +12,12 @@ import com.rpl.exception.RplQueueException;
 import com.rpl.model.ActivitySubmission;
 import com.rpl.model.QueueMessage;
 import com.rpl.model.Status;
+import com.rpl.persistence.ActivitySubmissionDAO;
 import com.rpl.service.QueueService;
 import com.rpl.service.SolutionService;
 import com.rpl.service.util.JsonUtils;
 
+@Stateless
 public class SolutionServiceImpl implements SolutionService {
 
 	@Inject
@@ -27,12 +30,11 @@ public class SolutionServiceImpl implements SolutionService {
 
 		submission.setSubmissionDate(new Date());
 		submission.setStatus(Status.PENDING);
-		
-		
+		submission = activitySubmissionDAO.save(submission);
 		
 		QueueMessage qm;
 		try {
-			qm = new QueueMessage(JsonUtils.objectToJson(submission));
+			qm = new QueueMessage(JsonUtils.objectToJson(submission.getId()));
 			queueService.send(qm);
 		} catch (JsonProcessingException e) {
 			// No deberia suceder nunca
