e9ff16c1157153f5ba49ef538ad88a4d73820df5
kevinmlew <kevin.martin.lew@gmail.com>
2016-11-09 22:27:34 -0300

    Mejoras en java wrapper

--- rpl-runner/extras/test-wrappers/java/Wrapper.java
+++ rpl-runner/extras/test-wrappers/java/Wrapper.java
@@ -1,20 +1,34 @@
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+
+import org.junit.Test;
 
 public class Wrapper {
 	
 	public static void main( String[] args ) {
 		Result result = JUnitCore.runClasses(TestSolution.class);
 
-		String resultStr = "{\"total\": " + result.getRunCount() + ", " +
-				"\"success\": " + (result.getRunCount() - result.getFailureCount()) + ", " +
-				"\"failure\": " + result.getFailureCount() + ", " + 
-				"\"tests\": [";
+		String resultStr = "{\"success\": " + result.wasSuccessful() + ", " +
+				"\"tests\": [";	
 		
+		List<String> testsFailed = new ArrayList<>();
 		for (Failure failure : result.getFailures()){
-			resultStr += "\"name\": \"" + failure.getDescription().getMethodName() + "\", " +
-				"\"error\": \"" + failure.getException().getClass().getName() + "\", ";
+			testsFailed.add(failure.getDescription().getMethodName());
+			resultStr += "{\"name\": \"" + failure.getDescription().getMethodName() + "\", " +
+				"\"success\": false, " +
+				"\"description\": \"" + failure.getException().getClass().getName() + "\"}, ";
+		}
+		for (Method method : TestSolution.class.getMethods()) {
+			if(method.isAnnotationPresent(Test.class) && !testsFailed.contains(method.getName())) {
+				resultStr += "{\"name\": \"" + method.getName() + "\", " +
+					"\"success\": true, " +
+					"\"description\": \"passed\"},";
+			}
 		}
 		resultStr += "]}";
 		System.out.println(resultStr);
