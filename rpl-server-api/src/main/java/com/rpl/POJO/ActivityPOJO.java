327b919acbd3a5aa219b62a9646addea857e72be
cdesseno <cdesseno@gmail.com>
2017-01-04 18:03:32 -0300

    change in pojo

--- rpl-server-api/src/main/java/com/rpl/POJO/ActivityPOJO.java
+++ rpl-server-api/src/main/java/com/rpl/POJO/ActivityPOJO.java
@@ -6,12 +6,16 @@ public class ActivityPOJO {
     private Long id;
     private String name;
     private String language;
+    private String description;
+    private String template;
     private int points;
 
     public ActivityPOJO(Activity activity) {
         this.id = activity.getId();
         this.name = activity.getName();
         this.language = activity.getLanguage().toString();
+        this.description = activity.getDescription();
+        this.template = activity.getTemplate();
         this.points = activity.getPoints();
     }
 
@@ -31,4 +35,12 @@ public class ActivityPOJO {
         return points;
     }
 
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
 }
