ac6cc1d3f91239656e95aa3f15cae2bf6c5646f5
AlJuan <juan6691@gmail.com>
2017-01-23 22:36:27 -0300

    Login returns role

--- rpl-server-api/src/main/java/com/rpl/POJO/TokenPOJO.java
+++ rpl-server-api/src/main/java/com/rpl/POJO/TokenPOJO.java
@@ -1,19 +1,26 @@
 package com.rpl.POJO;
 
+import com.rpl.model.Role;
+
 public class TokenPOJO {
 
 	private final String token;
+	private final Role role;
 
-	private TokenPOJO(String token){
+	private TokenPOJO(String token, Role rol){
 		this.token = token;
+		this.role = rol;
 	}
 	
-	public static TokenPOJO of (String token){
-		return new TokenPOJO(token);
+	public static TokenPOJO of (String token, Role rol){
+		return new TokenPOJO(token, rol);
 	}
 	
 	public String getToken() {
 		return token;
 	}
 	
+	public Role getRol() {
+		return role;
+	}
 }
