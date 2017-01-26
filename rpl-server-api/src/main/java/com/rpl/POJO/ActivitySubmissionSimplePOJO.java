311488cf0aa59d04f31d11a7bcc5837813fc5c14
kevinmlew <kevin.martin.lew@gmail.com>
2017-01-26 20:33:20 -0300

    API devuelve la submission seleccionada

--- rpl-server-api/src/main/java/com/rpl/POJO/ActivitySubmissionSimplePOJO.java
+++ rpl-server-api/src/main/java/com/rpl/POJO/ActivitySubmissionSimplePOJO.java
@@ -5,10 +5,12 @@ import com.rpl.model.ActivitySubmission;
 public class ActivitySubmissionSimplePOJO {
     private Long id;
     private String status;
+    private boolean selected;
 
     public ActivitySubmissionSimplePOJO(ActivitySubmission submission) {
         this.id = submission.getId();
         this.status = submission.getStatus().toString();
+        this.selected = submission.isSelected();
     }
 
     public Long getId() {
@@ -17,5 +19,10 @@ public class ActivitySubmissionSimplePOJO {
 
     public String getStatus() {
         return status;
+    }
+
+
+    public boolean isSelected() {
+        return selected;
     }
 }
