58bcc5ed8d156657714b6df8e297edc9afb14ced
AlJuan <juan6691@gmail.com>
2017-06-18 12:42:03 -0300

    error on person update

--- rpl-service/src/main/java/com/rpl/service/PersonService.java
+++ rpl-service/src/main/java/com/rpl/service/PersonService.java
@@ -6,7 +6,6 @@ import com.rpl.exception.RplException;
 import com.rpl.model.CoursePerson;
 import com.rpl.model.Person;
 import com.rpl.model.PersonImage;
-import com.rpl.model.Role;
 
 public interface PersonService {
 
@@ -14,8 +13,8 @@ public interface PersonService {
 	public Person getPersonById(Long id);
     public Person getPersonByUsername(String username);
 	public void addCoursePerson(CoursePerson coursePerson) throws RplException;
-	public void updatePersonInfo(Long id, String name, String mail, Long studentId);
-	public void updatePersonInfo(Long id, String name, String mail, Long studentId, String role);
+	public void updatePersonInfo(Long id, String name, String mail, Long studentId) throws RplException;
+	public void updatePersonInfo(Long id, String name, String mail, Long studentId, String role) throws RplException;
 	public CoursePerson getCoursePersonByIdAndCourse(Long personId, Long courseId);
 	public void saveImage(Long id, PersonImage courseImage) throws RplException;
 	public void deletePersonById(Long id);
