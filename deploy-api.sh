3ecac57bc797b0683b5a890dba733c739c6e41ae
martin RPL <martincura@gmail.com>
2018-11-19 05:46:25 +0000

    fix: Deploy server-api before compiling daemon

--- deploy-api.sh
+++ deploy-api.sh
@@ -6,6 +6,8 @@ cd $BASEDIR/repo
 mvn clean install
 sudo docker build -t rpl rpl-runner
 mvn clean install -PwebApp
-mvn package -PstandaloneApp
+
 cp $BASEDIR/repo/rpl-server-api/target/rpl-server-api.war $BASEDIR/wildfly/wildfly-10.1.0.Final/standalone/deployments/
 touch $BASEDIR/wildfly/wildfly-10.1.0.Final/standalone/deployments/rpl-server-api.war.dodeploy
+
+mvn package -PstandaloneApp
